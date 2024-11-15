59ff944148214b5cd689e8e2c6602cd63e3e7dce
kaiq-ts <kaiqt.souza@gmail.com>
2024-11-15 16:13:19 -0300

    responsable menu

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+//MENUBUTTON
+const btnMobile = document.getElementById ('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const isActive = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', isActive);
+  event.currentTarget.setAttribute('aria-label', isActive ? 'Fechar Menu' : 'Abrir Menu');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
 // BACKTOTOPBUTTON
 let backToTopButton = document.querySelector(".back-to-top");
 
