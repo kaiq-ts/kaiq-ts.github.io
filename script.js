050b5d375bbc572d8e0c6aecb23d1de9ee8e183f
kaiq-ts <kaiqt.souza@gmail.com>
2024-12-17 19:58:28 -0300

    code review

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
-// BACKTOTOPBUTTON
-let mybutton = document.getElementById("myBtn");
+const lenis = new Lenis({ autoRaf: true });
+let SwiperTop = new Swiper(".swiper--top", {
+  spaceBetween: 0,
+  centeredSlides: true,
+  speed: 1500,
+  autoplay: {
+    delay: 1,
+  },
+  loop: true,
+  slidesPerView: "auto",
+  allowTouchMove: false,
+  disableOnInteraction: true,
+});
+
+const backToTopButton = document.getElementById("back-to-top-button");
+const body = document.body;
+const documentElement = document.documentElement;
 
 window.onscroll = function () {
   scrollFunction();
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+  if (body.scrollTop > 20 || documentElement.scrollTop > 20) {
+    backToTopButton.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    backToTopButton.style.display = "none";
   }
 }
 
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  body.scrollTop = 0;
+  documentElement.scrollTop = 0;
 }
